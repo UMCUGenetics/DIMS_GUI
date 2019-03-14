567f1709c78bb24e06988c0ae63969acd817aa7c
Nienke <n.vanunen@umcutrecht.nl>
2019-03-14 12:26:29 +0100

    proteowizard version

--- dims/config.R
+++ dims/config.R
@@ -11,11 +11,11 @@ mail = "n.vanunen@umcutrecht.nl"
 #root = "C:/Xcalibur/data/Research"
 root = "Y:/Metabolomics/DIMS_pipeline/R_workspace_NvU"
 
-### Root for experimental design file selector 
+### Root for experimental design file selector (sample sheet)
 #root2 = "Y:/Metabolomics/Research Metabolomic Diagnostics/Metabolomics Projects"
 root2 = root
 
 ### Folders on HPC
 base = "/hpc/dbg_mz"
 scriptDir = "/production/DIMS"
-proteowizardDir = "/tools/proteowizard"
+proteowizardDir = "/tools/proteowizard_3.0.19056-6b6b0a2b4"
